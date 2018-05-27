6886d217871ea6de44e7164cfc8cd7b764c1c8a6
Tang Kha Duy NGUYEN <ntkduy1604@gmail.com>
2018-05-27 21:03:58 +0800

    Init commit

---
+++ playround/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err) {
+    console.log(`Unable to connect to Mongodb server`);
+  }
+  console.log(`Connected to the server`);
+  const db = client.db('TodoApp')
+
+  //  Insert new doc into Todos (text, completed)
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne({
+    name: 'Tang NGUYEN',
+    age: 31,
+    location: "Western Australia"
+  }, (err, result) => {
+    if(err) {
+      return console.log('Unable to insert new record', err);
+    }
+    console.log(result.ops);
+  })
+
+  client.close();
+});
